be9bebe39733171497dca760f8bd792c66d11ac4
Andrea <andreakwilliams1@gmail.com>
2019-11-09 11:16:12 -0500

    setup database

---
+++ models/Todo.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+module.exports = function(sequelize, DataTypes) {
+  const Todo = sequelize.define('Todo', {
+    text: DataTypes.STRING,
+    complete: DataTypes.BOOLEAN,
+  });
+  return Todo;
+};
+
